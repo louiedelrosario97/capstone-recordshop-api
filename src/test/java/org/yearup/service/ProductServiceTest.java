fd598c328d671fc2d219cce3677626313bff71ac
Louie D <louie.delrosario97@gmail.com>
2026-06-22 21:59:32 -0700

    started the code skeleton of Bug 1's UnitTest

--- src/test/java/org/yearup/service/ProductServiceTest.java
+++ src/test/java/org/yearup/service/ProductServiceTest.java
@@ -1,7 +1,31 @@
 package org.yearup.service;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.yearup.repository.ProductRepository;
+
 import static org.junit.jupiter.api.Assertions.*;
 // workbook 8a pg. 89 - how to write tests for Service Layer
-class ProductServiceTest {
+@ExtendWith(MockitoExtension.class)
+class ProductServiceTest
+{
+    @Mock
+    private ProductRepository productRepository;
+
+    @InjectMocks
+    private ProductService productService;
+
+    @Test
+    public void searchNoFiltersApplied_shouldReturnBoth_featuredAndNonFeaturedProducts()
+    {
+        // arrange
+        // act
+        // assert
+    }
+
+
 
 }
