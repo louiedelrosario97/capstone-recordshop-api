78ff4e61dd12d34b790262d0acc9b8bfe469a131
Louie D <louie.delrosario97@gmail.com>
2026-06-23 20:34:13 -0700

    working on Phase 3 of Shopping Cart. Currently trying to build the logic that takes the product data from CartItem and ProductService, then combines them into ShoppingCartItem

--- src/main/java/org/yearup/service/ShoppingCartService.java
+++ src/main/java/org/yearup/service/ShoppingCartService.java
@@ -1,8 +1,12 @@
 package org.yearup.service;
 
 import org.springframework.stereotype.Service;
+import org.yearup.models.CartItem;
+import org.yearup.models.Product;
 import org.yearup.models.ShoppingCart;
 import org.yearup.repository.ShoppingCartRepository;
+
+import java.util.List;
 
 @Service
 public class ShoppingCartService
@@ -19,8 +23,20 @@ public class ShoppingCartService
 
     public ShoppingCart getByUserId(int userId)
     {
-        // load the user's cart rows, look up each product, and build the ShoppingCart
-        return null;
+        // Instructions: load the user's cart rows, look up each product, and build the ShoppingCart
+
+        // we want data from our DB, so we create a list that calls the repository. (repo connects to DB)
+        List<CartItem> cartById = shoppingCartRepository.findByUserId(userId);
+
+        // CartItem's variables are just IDs, so we need to use the 'productId' to fetch the full data
+        // we'll use ProductService's getById() and pass in CartItem's getProductId()
+
+        for (CartItem cartItem : cartById)
+        {
+            Product product = productService.getById(cartItem.getProductId());
+        }
+
+        return
     }
 
     // add additional methods here
